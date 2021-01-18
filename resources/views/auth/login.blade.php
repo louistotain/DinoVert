88ae615fc9dadc68ef07dfe18d32bdd4fc73e881
Louis Totain <louis.totain@stcharles-stecroix.org>
2021-01-18 22:22:38 +0100

    Header + header logo jetstream

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img src="{{asset('img/logo_png.png')}}" height="100" width="100">
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
